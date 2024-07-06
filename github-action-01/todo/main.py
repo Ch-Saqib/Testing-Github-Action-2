37fa74de93d57fb7b39f06f4086ef01702c11302
Ch-Saqib17 <choudary1598@gmail.com>
2024-07-06 11:42:54 +0500

    Github Action 2

--- github-action-01/todo/main.py
+++ github-action-01/todo/main.py
@@ -1,26 +1,3 @@
-# from typing import Annotated, AsyncGenerator
-# from fastapi import FastAPI, Depends
-# from todo.models import *
-# from todo.database import *
-# from contextlib import asynccontextmanager
-# from todo.router import *
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI) -> AsyncGenerator:
-#     create_tables()
-#     yield
-
-
-# app: FastAPI = FastAPI(
-#     title="Todo App",
-#     description="A simple todo app ",
-#     version="0.1.0",
-#     lifespan=lifespan,
-# )
-
-# app.include_router(router,prefix="/todo",tags=["todo"])
-
-# app.include_router(product_router, prefix="/product", tags=["product"])
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -29,7 +6,5 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {
-        "Hello": "World",
-        "World2": "Pakistan Zindabad",
-        "Testing-Zone": "I Am From Testing Zone.",
+        "message":"I am From Github Action."
     }
